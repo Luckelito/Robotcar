26383509670dad5c573df9b200b6e955eb318e52
Lukas Mosskull <lukasmos@kth.se>
2022-07-16 00:16:35 +0200

    Change to pwm

--- Main.py
+++ Main.py
@@ -16,15 +16,19 @@ def main():
 
     GPIO.setmode(GPIO.BOARD)
 
-    LF_cable = 11 # brown: left forward
-    LB_cable = 13 # red: left backward
-    RF_cable = 16 # yellow: right forward
-    RB_cable = 15 # orange: right backward
+    LF_cable = 12 # brown: left forward
+    LB_cable = 32 # red: left backward
+    RF_cable = 35 # yellow: right forward
+    RB_cable = 33 # orange: right backward
+    rev_gun_cable = 38
+    shoot_cable = 40
 
-    GPIO.setup(LF_cable, GPIO.OUT)
+    LF_pwm = GPIO.PWM(LF_cable, 1_000)
+    RF_pwm = GPIO.PWM(RF_cable, 1_000)
     GPIO.setup(LB_cable, GPIO.OUT)
-    GPIO.setup(RF_cable, GPIO.OUT)
     GPIO.setup(RB_cable, GPIO.OUT)
+    GPIO.setup(rev_gun_cable, GPIO.OUT)
+    GPIO.setup(shoot_cable, GPIO.OUT)
 
     # for al the connected joysticks
     for i in range(0, pygame.joystick.get_count()):
@@ -35,46 +39,46 @@ def main():
         # print a statement telling what the name of the controller is
         print ("Detected joystick "),joysticks[-1].get_name(),"'"
 
-    is_running_LF = False
-    is_running_RF = False
-    is_running_LB = False
-    is_running_RB = False
+    LF_pwm.start(0)
+    RF_pwm.start(0)
+    #is_running_LF = False
+    #is_running_RF = False
+    #is_running_LB = False
+    #is_running_RB = False
     while keep_playing:    
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == 7: # Axis event
                 if event.axis == 5:
-                    if int(event.value * 1000) != -1000 and not is_running_LB:
-                        is_running_LF = True
-                        cable_on(LF_cable)
-                    else:
-                        is_running_LF = False
-                        cable_off(LF_cable)
+                    LF_pwm.ChangeDutyCycle(event.value * 50 + 50)
                 if event.axis == 4:
-                    if int(event.value * 1000) != -1000 and not is_running_RB:
-                        is_running_RF = True
-                        cable_on(RF_cable)
-                    else:
-                        is_running_RF = False
-                        cable_off(RF_cable)
+                    RF_pwm.ChangeDutyCycle(event.value * 50 + 50)
 
             elif event.type == 10: # Button down event
-                if event.button == 6 and not is_running_LF:
-                    is_running_LB = True
-                    cable_on(LB_cable)
-                if event.button == 7 and not is_running_RF:
-                    is_running_RB = True
-                    cable_on(RB_cable)
-                if event.button == 1: # Use button b to terminate program
+                #if event.button == 6 and not is_running_LF:
+                #    is_running_LB = True
+                #    cable_on(LB_cable)
+                #if event.button == 7 and not is_running_RF:
+                #    is_running_RB = True
+                #    cable_on(RB_cable)
+                if event.button == 11: # Use menu button to terminate program
                     keep_playing = False
+                if event.button == 0: # A button
+                    cable_on(rev_gun_cable)
+                if event.button == 1: # B button
+                    cable_on(shoot_cable)
 
             elif event.type == 11: # Button up event
-                if event.button == 6:
-                    is_running_LB = False
-                    cable_off(LB_cable)
-                if event.button == 7:
-                    is_running_RB = False
-                    cable_off(RB_cable)
+                #if event.button == 6:
+                #    is_running_LB = False
+                #    cable_off(LB_cable)
+                #if event.button == 7:
+                #    is_running_RB = False
+                #    cable_off(RB_cable)
+                if event.button == 0: # A button
+                    cable_off(rev_gun_cable)
+                if event.button == 1: # B button
+                    cable_off(shoot_cable)
                     
 if __name__ == "__main__":
     try:
